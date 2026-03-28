205b161240139cb7c574e86e6cb0178de5918210
Shah Prachi <200120116021@git.org.in>
2026-03-28 17:05:18 +0530

    feat: changing timezone to asia/kolkata (local) instead of utc timezone"

--- expensepro/settings.py
+++ expensepro/settings.py
@@ -237,6 +237,7 @@ SPECTACULAR_SETTINGS = {
 # ==================================================
 
 LANGUAGE_CODE = "en-us"
-TIME_ZONE = "UTC"
+# TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Kolkata"
 USE_I18N = True
 USE_TZ = True
